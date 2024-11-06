85d76b2ef7731dc83ada23ca04c697e57c0c36a9
Retno Ires <fairy.edensor@gmail.com>
2024-11-06 09:51:33 +0700

    add:raport

---
+++ Raport/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std; 
+
+int main() {
+	int nilaiBing, nilaiBin, nilaiPro, nilaiMtk; 
+	
+	cout<<"Inputkan nilai Bahasa Inggris ";
+	cin>>nilaiBing; 
+	
+	return 0;
+}
